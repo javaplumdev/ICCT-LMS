34da018b013dd6f047dd86df7d7bc66d0d83b0ae
Charlito Mecarez Jr <70713746+javaplumdev@users.noreply.github.com>
2022-04-02 12:07:51 +0800

    update

--- LMS-SYSTEM/class page/class_page.js
+++ LMS-SYSTEM/class page/class_page.js
@@ -1,8 +1,13 @@
 // Importing the feed data
 import { subjectsAvailable } from '../subjectsAvailable.js';
+import { feedData } from '../feedData.js';
 
 // Selectors
 const feedContainer = document.querySelector('.feed-container');
+const enrollBtn = document.querySelector('.enroll-btn');
+const modalBody = document.querySelector('.modal-body');
+const modalTitle = document.querySelector('.modal-title');
+const viewSubjectButton = document.getElementsByClassName('view-subject');
 
 // Show modal
 var myModal = new bootstrap.Modal(document.getElementById('myModal'));
@@ -37,15 +42,10 @@ filteredArr.forEach((props) => {
 });
 
 function goToPage() {
-	const viewSubjectButton = document.getElementsByClassName('view-subject');
-
 	for (let i = 0; i < viewSubjectButton.length; i++) {
 		const button = viewSubjectButton[i];
 
 		button.addEventListener('click', function () {
-			const modalBody = document.querySelector('.modal-body');
-			const modalTitle = document.querySelector('.modal-title');
-
 			modalTitle.innerHTML = subjectsAvailable[i].subjectName;
 
 			modalBody.innerHTML = `
@@ -57,6 +57,41 @@ function goToPage() {
 		});
 	}
 }
+
+enrollBtn.addEventListener('click', function (event) {
+	const buttonClicked = event.target;
+	const modalSubjectName =
+		buttonClicked.parentElement.parentElement.children[0].children[0].innerHTML;
+
+	feedData.forEach((data) => {
+		if (data.subjectName === modalSubjectName) {
+			modalBody.innerHTML = `
+                <p>You are already enrolled in this subject. Please check your list of subjects page.</p>
+            `;
+		} else {
+			modalBody.innerHTML = `
+                <p>Congratulations! You are now enrolled. Please check your list of subjects page.</p>
+            `;
+
+			subjectsAvailable.forEach((data) => {
+				if (data.subjectName === modalSubjectName) {
+					feedData.push(data);
+				}
+			});
+		}
+	});
+
+	const filteredArr = feedData.reduce((acc, current) => {
+		const x = acc.find((item) => item.subjectName === current.subjectName);
+		if (!x) {
+			return acc.concat([current]);
+		} else {
+			return acc;
+		}
+	}, []);
+
+	console.log(filteredArr);
+});
 
 //Calling the functions
 goToPage();
--- LMS-SYSTEM/feedData.js
+++ LMS-SYSTEM/feedData.js
@@ -90,4 +90,49 @@ export let feedData = [
 			},
 		],
 	},
+	{
+		keyId: '3',
+		subjectName: 'Sample subject',
+		quizDetails: {
+			chapter: 1,
+			instruction:
+				'MULTIPLE CHOICE. Read and understand the statement carefully and choose the letter of the correct answer.',
+			title: 'Basic computing',
+			quizLengthTime: 5,
+		},
+		subjectLink: '../subjects page/subject_page.html',
+		quizLink: '../quiz page/quiz_page.html',
+		quizLinkAtHomePage: './quiz page/quiz_page.html',
+		questions: [
+			{
+				questionNum: 1,
+				question:
+					'There are 49 dogs signed up for a dog show. There are 36 more small dogs than large dogs. How many small dogs have signed up to compete?',
+				a: '42.5',
+				b: '6.9',
+				c: '9',
+				d: '6',
+				correct: 'a',
+			},
+			{
+				questionNum: 2,
+				question: 'Add 8.563 and 4.8292.',
+				a: '922',
+				b: '133',
+				c: '13.3922',
+				d: '339',
+				correct: 'c',
+			},
+			{
+				questionNum: 3,
+				question:
+					'Using only an addition, how do you add eight 8s and get the number 1000?',
+				a: '1000',
+				b: '888',
+				c: '41',
+				d: '54 ',
+				correct: 'a',
+			},
+		],
+	},
 ];
